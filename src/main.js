442e8450b12c7055bd5e196fc0f592fb04bdd430
pd4d10 <pd4d10@gmail.com>
2016-10-04 22:10:34 +0800

    add parse and stringify

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-var hosts = MacGap.File.read('/etc/hosts', 'string');
+const hosts = MacGap.File.read('/etc/hosts', 'string');
 
-document.write(hosts)
+const parseHost = host => {
+  return host.split('\n')
+    .map(x => x.split(/\s+/))
+    .reduce((a, b) => {
+      return Object.assign(a, {
+        [b[1]]: b[0]
+      })
+    }, {})
+}
+
+const stringifyHost = json => {
+  return Object.keys(json)
+    .map(key => `${json[key]} ${key}`)
+    .join('\n')
+}
