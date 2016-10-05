9e82cde54a00ef927b4613272669ab3d5fdc26f7
pd4d10 <pd4d10@gmail.com>
2016-10-05 10:01:23 +0800

    add more test

--- test/util.js
+++ test/util.js
@@ -1,23 +1,43 @@
 import test from 'ava'
 import { parse, stringify } from '../src/util'
 
-const host = '127.0.0.1 localhost'
-const json = {
-  localhost: '127.0.0.1'
+const data = [
+  {
+    message: 'normal',
+    host: '127.0.0.1 localhost',
+    json: {
+      localhost: '127.0.0.1'
+    }
+  },
+  {
+    message: 'multiple space',
+    host: '127.0.0.1  localhost',
+    json: {
+      localhost: '127.0.0.1'
+    },
+    sHost: '127.0.0.1 localhost'
+  },
+  {
+    message: 'multiple line',
+    host: '127.0.0.1 localhost\n192.168.0.1 local',
+    json: {
+      localhost: '127.0.0.1',
+      local: '192.168.0.1'
+    }
+  }
+]
+
+/**
+ * parse then stringify
+ */
+const testPair = ({ message, host, json, sHost }) => {
+  test(`parse: ${message}`, t => {
+    t.deepEqual(parse(host), json)
+  })
+
+  test(`stringify: ${message}`, t => {
+    t.deepEqual(stringify(json), sHost || host)
+  })
 }
 
-test('parse', t => {
-  t.deepEqual(
-    parse(host),
-    json,
-    'should parse host correctly'
-  )
-})
-
-test('stringify', t => {
-  t.deepEqual(
-    stringify(json),
-    host,
-    'should stringify host correctly'
-  )
-})
+data.forEach(testPair)
